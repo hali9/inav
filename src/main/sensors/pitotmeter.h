f452079d206e8b429601848eb1b1164b78949bbe
hali9 <hali9@wp.pl>
2019-01-08 10:41:45 +0100

    Update pitotmeter.h

--- src/main/sensors/pitotmeter.h
+++ src/main/sensors/pitotmeter.h
@@ -59,8 +59,7 @@ typedef struct pito_s {
 #ifdef USE_PITOT
 
 #define AIR_DENSITY_SEA_LEVEL_15C   1.225f      // Air density at sea level and 15 degrees Celsius
-#define AIR_GAS_CONST               287.1f      //  J / (kg * K)
-#define P0                          101325.0f   // standard pressure
+#define P0                          101325.0f   // standard pressure [Pa]
 
 extern pitot_t pitot;
 
