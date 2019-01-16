e321bc973aecb0e616d8cdf5800b568041d1e874
hali9 <hali9@wp.pl>
2019-01-16 23:05:31 +0100

    Yaw navigation (#18)

--- src/main/navigation/navigation_fixedwing.c
+++ src/main/navigation/navigation_fixedwing.c
@@ -412,7 +412,9 @@ static void updatePositionHeadingController_FW(timeUs_t currentTimeUs, timeDelta
     rollAdjustment = pt1FilterApply4(&fwPosControllerCorrectionFilterState, rollAdjustment, NAV_FW_ROLL_CUTOFF_FREQUENCY_HZ, US2S(deltaMicros));
 
     // Convert rollAdjustment to decidegrees (rcAdjustment holds decidegrees)
-    posControl.rcAdjustment[ROLL] = CENTIDEGREES_TO_DECIDEGREES(rollAdjustment);
+    posControl.rcAdjustment[ROLL] = CENTIDEGREES_TO_DECIDEGREES(rollAdjustment) * pidProfile()->heading_to_roll / 100;
+
+    posControl.rcAdjustment[YAW] = CENTIDEGREES_TO_DECIDEGREES(rollAdjustment) * pidProfile()->heading_to_yaw / 100;
 }
 
 void applyFixedWingPositionController(navigationFSMStateFlags_t navStateFlags, timeUs_t currentTimeUs)
@@ -521,6 +523,14 @@ void applyFixedWingPitchRollThrottleController(navigationFSMStateFlags_t navStat
         // ROLL >0 right, <0 left
         int16_t rollCorrection = constrain(posControl.rcAdjustment[ROLL], -DEGREES_TO_DECIDEGREES(navConfig()->fw.max_bank_angle), DEGREES_TO_DECIDEGREES(navConfig()->fw.max_bank_angle));
         rcCommand[ROLL] = pidAngleToRcCommand(rollCorrection, pidProfile()->max_angle_inclination[FD_ROLL]);
+
+        //int16_t yawCorrection = constrain(posControl.rcAdjustment[YAW], -DEGREES_TO_DECIDEGREES(navConfig()->fw.max_bank_angle), DEGREES_TO_DECIDEGREES(navConfig()->fw.max_bank_angle));
+		//rcCommand[YAW] = pidAngleToRcCommand(yawCorrection, pidProfile()->max_angle_inclination[FD_ROLL]);
+        int16_t yawCorrection = constrain(posControl.rcAdjustment[YAW], 
+                                         -DEKADEGREES_TO_DECIDEGREES(currentControlRateProfile->stabilized.rates[FD_YAW]),
+                                          DEKADEGREES_TO_DECIDEGREES(currentControlRateProfile->stabilized.rates[FD_YAW]));
+		rcCommand[YAW] = pidRateToRcCommand(DECIDEGREES_TO_DEKADEGREES(yawCorrection),
+                                            currentControlRateProfile->stabilized.rates[FD_YAW]);
     }
 
     if (isPitchAdjustmentValid && (navStateFlags & NAV_CTL_ALT)) {
