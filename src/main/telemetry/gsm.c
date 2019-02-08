f2eaece736aeb810effcdf6f24a5db989febc64b
hali9 <hali9@wp.pl>
2019-02-08 12:45:34 +0100

    Update gsm.c

--- src/main/telemetry/gsm.c
+++ src/main/telemetry/gsm.c
@@ -1,11 +1,35 @@
+/*
+ * This file is part of INAV.
+ *
+ * INAV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INAV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with INAV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
 #include "build/build_config.h"
+#include "build/debug.h"
 
 #include "common/axis.h"
 #include "common/streambuf.h"
 #include "common/utils.h"
 #include "common/printf.h"
+#include "common/string_light.h"
 
+#include "drivers/light_led.h"
 #include "drivers/serial.h"
+#include "drivers/serial_uart.h"
 #include "drivers/time.h"
 
 #include "fc/config.h"
@@ -22,7 +46,6 @@
 #include "io/gps.h"
 #include "io/ledstrip.h"
 #include "io/serial.h"
-#include "drivers/serial_uart.h"
 
 #include "navigation/navigation.h"
 
@@ -40,14 +63,7 @@
 #include "telemetry/gsm.h"
 #include "telemetry/telemetry.h"
 
-#include "drivers/light_led.h"
-
-#include "common/string_light.h"
-
-#include <stdlib.h>
-#include <string.h>
-
-#include "build/debug.h"
+#if defined(USE_TELEMETRY) && defined(USE_TELEMETRY_GSM)
 
 static serialPortConfig_t *portConfig;
 static serialPort_t *gsmPort;
@@ -229,3 +245,5 @@ void checkGsmTelemetryState(void) {
     if (!gsmPort) return;
     gsmEnabled = true;
 }
+
+#endif
