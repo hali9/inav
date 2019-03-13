edaa9fe1af2c97d5b52b4d23c2702cdcf0760489
hali9 <hali9@wp.pl>
2019-03-13 08:45:36 +0100

    Update servos.c

--- src/main/flight/servos.c
+++ src/main/flight/servos.c
@@ -237,7 +237,7 @@ void servoMixer(float dT)
     input[INPUT_STABILIZED_YAW_MINUS] = constrain(input[INPUT_STABILIZED_YAW], -1000, 0);
 
     input[INPUT_FEATURE_FLAPS] = FLIGHT_MODE(FLAPERON) ? servoConfig()->flaperon_throw_offset : 0;
-    input[INPUT_FEATURE_SPOILERS] = FLIGHT_MODE(SPOILERON) ? servoConfig()->spoileron_throw_offset : 0;
+    input[INPUT_FEATURE_SPOILERS] = FLIGHT_MODE(SPOILERON) ? -servoConfig()->spoileron_throw_offset : 0;
 
     if (IS_RC_MODE_ACTIVE(BOXCAMSTAB)) {
         input[INPUT_GIMBAL_PITCH] = scaleRange(attitude.values.pitch, -900, 900, -500, +500);
