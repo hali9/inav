cf58ce5a3db8b9d3cb9946fe1de8d944a757f916
hali9 <hali9@wp.pl>
2019-03-30 20:54:22 +0100

    Fixes

--- src/main/target/common.h
+++ src/main/target/common.h
@@ -18,7 +18,7 @@
 #pragma once
 
 #if defined(STM32F3)
-#define DYNAMIC_HEAP_SIZE   1024
+#define DYNAMIC_HEAP_SIZE   1536
 #else
 #define DYNAMIC_HEAP_SIZE   2048
 #endif
@@ -101,7 +101,6 @@
 #define USE_BOOTLOG
 #define BOOTLOG_DESCRIPTIONS
 
-#define NAV_NON_VOLATILE_WAYPOINT_CLI
 #else // FLASH_SIZE < 256
 #define LOG_LEVEL_MAXIMUM LOG_LEVEL_ERROR
 #endif
@@ -122,6 +121,7 @@
 #define USE_GPS_PROTO_MTK
 #define NAV_AUTO_MAG_DECLINATION
 #define NAV_GPS_GLITCH_DETECTION
+#define NAV_NON_VOLATILE_WAYPOINT_CLI
 #define NAV_NON_VOLATILE_WAYPOINT_STORAGE
 #define USE_TELEMETRY_HOTT
 #define USE_TELEMETRY_IBUS
@@ -150,9 +150,7 @@
 #define USE_VTX_TRAMP
 #define USE_VTX_FFPV
 
-#ifndef STM32F3 //F3 series does not have enoug RAM to support logic conditions
 #define USE_LOGIC_CONDITIONS
-#endif
 
 //Enable DST calculations
 #define RTC_AUTOMATIC_DST
