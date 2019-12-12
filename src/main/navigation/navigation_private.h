753e49fa39bafd08889a01c8c4164148d5a311d1
hali9 <hali9@wp.pl>
2019-12-12 21:47:03 +0100

    Update navigation_private.h

--- src/main/navigation/navigation_private.h
+++ src/main/navigation/navigation_private.h
@@ -320,7 +320,7 @@ enum {
     NAV_TRACK_TYPE_LOITER_LAND = 2,
     NAV_TRACK_TYPE_STRAIGHT = 3,
     NAV_TRACK_TYPE_VIRTUAL = 4,
-}
+};
 
 typedef struct {
     /* Flags and navigation system state */
@@ -363,7 +363,7 @@ typedef struct {
     navWaypointPosition_t       activeWaypoint;     // Local position and initial bearing, filled on waypoint activation
     int8_t                      activeWaypointIndex;
 
-    navWaypointPosition_t       lastWaypoint;
+    fpVector3_t                 wpInitialPos;
     uint8_t                     trackType;
 
     /* Internals & statistics */
